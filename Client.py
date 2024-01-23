ac40e69bf5a6607e1e3f4cc452e87a04dd8d0f99
Narges <NJcesFUM@gmail.com>
2024-01-24 00:38:03 +0330

    add upload & response(in Server) function

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import socket
 from Commands import Command
+import os
 
 iP = '127.0.0.1'
 port = 2121
@@ -19,27 +20,59 @@ def help():
     print("\n===============Helper==============\n")
     print("\nyou can enter these command now to communicate with server or ", end="")
 
-def connect():
+def connect(command):
     #client connect to server 
     client.connect(ADDR)
+    client.send(command.encode(FORMAT))
     print(f"Client connect to server on {ADDR}\n")
 
+def upload(path, command):
+    if (not os.path.exists(path)):
+        print("file dosn't exist!")
+        return
+    
+    client.send(command.encode(FORMAT))
+
+    msg = client.recv(SIZE).decode
+    print(msg, end="\n")
+
+    filename = input("please enetr name for uploaded file: ")
+    filetype = input("please enetr type for uploaded file: ")
+    client.send(filename.encode(FORMAT))
+    client.send(filetype.encode(FORMAT))
+
+    msg = client.recv(SIZE).decode
+    print(msg, end="\n")
+
+    file = open(path, "r")
+    data = file.read()
+    client.send(data.encode(FORMAT))
+
+    msg = client.recv(SIZE).decode
+    print(msg, end="\n")
+
+    file.close()
+
+    
 while True:
     print(f"enter \"{Command['HELP'].value}\" to see command or \"{Command['QUIT'].value}\" if want to exit program\n")
     inp = input()
-    if (Command.Iscommand(inp) and is_connected):
-        print("YSE")
+    if inp == Command['QUIT'].value and is_connected:
         client.send(inp.encode(FORMAT))
-    if inp == Command['QUIT'].value:
         break
-    if inp == Command['HELP'].value:
+    elif inp == Command['HELP'].value:
         help()
-    if inp == Command['CONNECT'].value:
+    elif inp == Command['CONNECT'].value:
         print(is_connected)
         if (not is_connected):
-            connect()
+            connect(inp)
             is_connected = True
         else: print("you have already connected to the server!!")
+        recv = client.recv(SIZE).decode(FORMAT)
+        print(recv)
+    elif inp == Command['UPLOAD'].value and is_connected:
+        path = input("please enetr file path you want to upload: ")
+        upload(path, inp)
 
 
 client.close()
--- Server.py
+++ Server.py
@@ -6,6 +6,38 @@ port = 2121
 ADDR = (iP, port)
 FORMAT = "utf-8"
 SIZE = 1024
+PATH = ""
+
+def upload():
+    msg = "send filename"
+    connection.sendto(msg.encode(FORMAT), ADDR)
+
+    filename = connection.recv(SIZE).decode(FORMAT)
+    filetype = connection.recv(SIZE).decode(FORMAT)
+    file = open(PATH+filename+filetype, "w")
+
+    msg = "send data"
+    connection.sendto(msg.encode(FORMAT), ADDR)
+
+    data = connection.recv(SIZE).decode(FORMAT)
+    file.write(data)
+
+    msg = "upload file seccesfully finished"
+    connection.sendto(msg.encode(FORMAT), ADDR)
+
+    file.close()
+
+def response():
+    while True:
+        request = connection.recv(SIZE).decode(FORMAT)
+        if (request == Command['QUIT'].value):
+            print("Client disconnected")
+            break
+        elif (request == Command['CONNECT'].value):
+            msg = f"you are now in \"{PATH}\""
+            connection.sendto(msg.encode(FORMAT), ADDR)
+        elif (request == Command['UPLOAD'].value):
+            upload()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -15,8 +47,6 @@ while True:
     print(f"Srever is listening on {ADDR}\n")
     connection, addr = server.accept()
     print(f"Client connect on {addr}\n")
+    PATH = "server_data/"
 
-    request = connection.recv(SIZE).decode(FORMAT)
-    if (request == Command['QUIT'].value):
-        print("Client disconnected")
-        break
+    response()
